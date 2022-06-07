f8e7b344337c2f8645103962c94e9856b1c7a0dd
shikha <69755719+creater-shikha@users.noreply.github.com>
2022-06-08 00:39:55 +0530

    Create Z-Function.cpp

---
+++ StringII/Z-Function.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.length()>haystack.length()){
+            return -1;
+        }
+        int j=0 ;
+      for(int i=0;i<haystack.length();i++){
+            if(haystack[i]==needle[j]){
+                j++;}
+            else{
+                i=i-j;
+                j=0;
+            }
+            if(j==needle.length()){
+                return i-j+1;
+            }
+      }
+            
+         return -1;
+    }
+};
