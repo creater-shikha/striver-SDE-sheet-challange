a0b7fe1f74f7b9e079fee183945806d55fa0ec67
shikha <69755719+creater-shikha@users.noreply.github.com>
2022-06-08 06:34:51 +0530

    actually que are interchanged so correct it;

--- StringII/KMP algo_ LPS(pi) array.cpp
+++ StringII/KMP algo_ LPS(pi) array.cpp
@@ -1,6 +1,5 @@
-int zAlgorithm(string s, string p, int n, int m)
-{int ans=0;
- if(p.length()>s.length())return false;
+bool findPattern(string p, string s)
+{ if(p.length()>s.length())return false;
     int j=0;
     for(int i=0;i<s.length();i++){
         if(s[i]==p[j]){
@@ -12,9 +11,9 @@ int zAlgorithm(string s, string p, int n, int m)
         
         }
         if(j==p.length()){
-           
-           ans++;
+            return true;
+            break;
         }
     }
-    return ans;
+    return false;
 }
