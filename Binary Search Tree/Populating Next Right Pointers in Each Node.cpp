79edb89f24d1cd425ec2425b75db0375beca4b9b
shikha <69755719+creater-shikha@users.noreply.github.com>
2022-06-20 11:54:05 +0530

    Create Populating Next Right Pointers in Each Node.cpp

---
+++ Binary Search Tree/Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root==NULL)return root;
+        Node* curr=root;
+        while(curr->left!=NULL){
+            Node*temp=curr;
+            while(curr!=NULL){
+            curr->left->next=curr->right;
+           curr->right->next= curr->next==NULL?NULL:curr->next->left;
+          curr=curr->next;
+        }
+            curr=temp->left;
+        }
+        return root;
+    }
+};
