a37be54021a9f7a64f3ae58b492e475dd4d3c0a0
shikha <69755719+creater-shikha@users.noreply.github.com>
2022-07-02 08:23:43 +0530

    Create Find LCA of two nodes in BST.cpp

---
+++ Binary serch tree/Find LCA of two nodes in BST.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // if(root==NULL||root->val==p->val||root->val==q->val)return root;
+        // if(root->val>p->val&& root->val<q->val)return root;
+        // if(root->val>p->val){
+        //     return lowestCommonAncestor(root->left, p,q);
+        // }
+        //  if(root->val<q->val){
+        //     return lowestCommonAncestor(root->right, p,q);
+        // }
+        if(root==NULL){
+            return root;
+        }
+         if(root->val>p->val &&root->val>q->val){
+            return lowestCommonAncestor(root->left, p,q);
+        }
+         if(root->val<q->val&&root->val<p->val){
+            return lowestCommonAncestor(root->right, p,q);
+        }
+        
+        
+        
+        
+    return root;
+    }
+};
